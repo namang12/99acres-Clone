3bb0dfbccd1db7dc827e61ba249f5a07c892b1c5
rohilashruti <rohilashruti@gmail.com>
2023-09-05 17:11:04 +0530

    Added Single Property page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Listing from "./pages/Listing";
+import { SingleP } from "./components/SingleProperty/SingleP";
 import Properties from "./pages/Properties";
 import Planpage from "./pages/Planpage";
 
 function App() {
   return (
+    
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/postproperty" element={<Listing />} />
         <Route path="/properties" element={<Properties />} />
         <Route path="/postproperty" element={<Listing/>} />
+        <Route path="/singlep" element={<SingleP/>} />
         <Route path="/subscription" element={<Planpage/>} />
       </Routes>
     </BrowserRouter>
